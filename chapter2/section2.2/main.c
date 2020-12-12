3cacc65317754ce6c92b153a00940a133fd29035
Tsupko, Alexander <Aleksandr.Tsupko@x5.ru>
2020-12-12 12:57:38 +0300

    add primality testing algorithm

--- chapter2/section2.2/main.c
+++ chapter2/section2.2/main.c
@@ -3,17 +3,35 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
     printf("Number of arguments: %d\n", argc);
 
-    if (argc == 3)
-    {
+    if (argc == 4) {
         for (int i = 0; i < argc; i++) {
             printf("Command-line argument is: %s\n", argv[i]);
         }
+
+        // Алгоритм определения простоты числа, переданного аргументом argv[3]
+        int number = atoi(argv[3]); // todo migrate from atoi() to strtol()
+        if (number > 1) {
+            printf("Testing for primality the number: %d\n", number);
+            bool isPrime = true;
+            for (int factor = 2; factor < number; factor++) {
+                if (number % factor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            printf(isPrime ? "Number %d is prime\n" : "Number %d is not prime\n", number);
+        } else {
+            printf("Number 1 is neither prime nor composite\n");
+        }
+    } else {
+        printf("Usage: ./main arg[1] arg[2] arg[3]\n");
     }
-    else printf("Usage: ./main arg[0] arg[1]");
 
     return 0;
 }
